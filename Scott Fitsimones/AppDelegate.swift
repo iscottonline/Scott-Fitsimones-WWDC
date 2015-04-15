ff471f7ade63e959d86b5b70ad2d8249ab74fe58
Scott Fitsimones <Scott@Scotts-MacBook-Pro.local>
2015-04-14 17:40:40 -0700

    Initial Commit

---
+++ Scott Fitsimones/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  Scott Fitsimones
+//
+//  Created by Scott Fitsimones on 4/14/15.
+//  Copyright (c) 2015 Bolt Visual. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(aNotification: NSNotification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(aNotification: NSNotification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ Scott Fitsimones/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  Scott Fitsimones
+//
+//  Created by Scott Fitsimones on 4/14/15.
+//  Copyright (c) 2015 Bolt Visual. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: AnyObject? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
